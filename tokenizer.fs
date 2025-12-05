5dea60f05b444e082c9fc7fe2c339b786ce87a0e
aliaa-anx <Aliaa_20220302@fci.helwan.edu.eg>
2025-12-05 05:24:06 +0200

    integration test for first three tasks done

--- tokenizer.fs
+++ tokenizer.fs
@@ -15,7 +15,7 @@ module Tokenizer=
   //1.word tokenizer
     let getwords(text:string)=
         // space, !, , . ? ; \t \n
-        let separators=[|' ' ; '!'; ','; '.' ; '?'; ';' ;';' ; '\t' ;'\n'|]
+        let separators=[|' ' ; '!'; ','; '.' ; '?'; ';' ;';' ; '\t' ;'\n';'\r'|]
         text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
         |>Array.toList
    //2.Sentence tokenizer
