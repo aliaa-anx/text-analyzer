8b33bf74d372f5b0734017b7b13dda9d7ac46f81
aliaa-anx <Aliaa_20220302@fci.helwan.edu.eg>
2025-12-05 03:46:55 +0200

    Added Tokenizer module

---
+++ Program.fs
@@ -0,0 +1,41 @@
+﻿//module for tokenizing text into words, sentences, and paragraphs
+
+open System
+//whole module gets called by no.4 text statistics
+//task no.5 frequency analysis calls function getwords only
+
+type TokenizedText =
+    { Words: string list
+      Sentences: string list
+      Paragraphs: string list } 
+
+      
+module Tokenizer=
+
+  //1.word tokenizer
+    let getwords(text:string)=
+        // space, !, , . ? ; \t \n
+        let separators=[|' ' ; '!'; ','; '.' ; '?'; ';' ;';' ; '\t' ;'\n'|]
+        text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+        |>Array.toList
+   //2.Sentence tokenizer
+    let getsentences(text:string)=
+          // . ! ?
+        let separators=[|'.' ; '!'; '?'|]
+        text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+        |>Array.map(fun s->s.Trim())
+        |>Array.toList
+
+   //3.Paragraph tokenizer
+    let getparagraphs(text:string)=
+          // \n\n
+        let separators=[|'\n';'\r'|]
+        text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+        |>Array.map(fun s->s.Trim())
+        |>Array.toList
+
+    let tokenize (text: string) : TokenizedText =
+        { Words = getwords text
+          Sentences = getsentences text
+          Paragraphs = getparagraphs text }
+
